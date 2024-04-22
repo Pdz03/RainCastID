e743aba04047844d28050a63bcbf94bfb41af295
Syaifudin Fendi Prasetyo <syaifudinfendip@gmail.com>
2024-04-23 04:56:09 +0700

    add tensorflow

--- predict.py
+++ predict.py
@@ -4,7 +4,7 @@ from datetime import datetime, timedelta
 from werkzeug.utils import secure_filename
 import pandas as pd
 import numpy as np
-import keras as tfkeras
+import tensorflow.keras as tfkeras
 
 class predictFunction:
     def ambildata():
