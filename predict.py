0b9edcad5da768279368bd91a5ad3d82359eb0e5
Syaifudin Fendi Prasetyo <syaifudinfendip@gmail.com>
2024-04-22 21:16:31 +0700

    fix module 2

--- predict.py
+++ predict.py
@@ -6,7 +6,7 @@ import pandas as pd
 import numpy as np
 import os
 os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
-import tensorflow.keras as tfkeras
+import keras as tfkeras
 
 class predictFunction:
     def ambildata():
